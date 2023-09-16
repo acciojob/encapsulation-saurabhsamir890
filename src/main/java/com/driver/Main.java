5fff0c55b3f9e999d1ba1bcd5c7b0c6686883077
unknown <srbhsamir6@gmail.com>
2023-09-16 17:28:13 +0530

    attempt1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+
+
+    public static void main(String[] args){
+
+        RWOnly obj = new RWOnly();
+
+        //System.out.println(obj.name="Saurabh");
+        //'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("Saurabh");
+
+        System.out.println(obj.getName());
+
+    }
+
+
+
+
 }
